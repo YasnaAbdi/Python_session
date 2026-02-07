44cef689bea52103c143c386e320d1c7c50d6e9f
YasnaAbdi <yasnaabdi2003@gmail.com>
2026-02-07 21:42:10 +0330

    Turtle Crossing

---
+++ turtle-crossing/car_manager.py
@@ -0,0 +1,15 @@
+import random
+from turtle import Turtle
+import random
+ 
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shapesize(20, 40)
+        self.color(random.choice[COLORS])
+
