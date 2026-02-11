913abb28ec116363b0a0ac9758d78f265bc90521
YasnaAbdi <yasnaabdi2003@gmail.com>
2026-02-11 22:52:11 +0330

    Letter to Avatar

---
+++ letterToAvatar/main.py
@@ -0,0 +1,12 @@
+
+
+with open("./Names/invited_names.txt", "r") as names:
+    name = names.readlines()
+
+with open("./Input/Letters/starting_letter.txt", "r") as letter:
+    letters = letter.read()
+    for n in name:
+        new_letter = (letters.replace("[name]", n))
+
+with open("./Output/ReadyToSend", "w") as letterTo:
+    pass
