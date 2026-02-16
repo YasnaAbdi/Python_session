bf6cabbe0575497b706e75e5811d26a1d50079ea
YasnaAbdi <yasnaabdi2003@gmail.com>
2026-02-16 20:17:10 +0330

    Cookie Clicker

--- day48/main.py
+++ day48/main.py
@@ -7,11 +7,11 @@ chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.python.org/")
-# price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
-# price_cent = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
-# print(f"The price is {price_dollar.text}.{price_cent.text}")
-#
-# driver.close()
+price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+price_cent = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
+print(f"The price is {price_dollar.text}.{price_cent.text}")
+
+driver.close()
 events = {}
 event_times = driver.find_elements(By.CSS_SELECTOR, value=".event-widget time")
 event_year = driver.find_elements(By.CSS_SELECTOR, value=".event-widget time span")
